390ab6e3690f1cf4d0c79a128f562eff9f5017cf
Kaan Alpar <kaanalpar33@gmail.com>
2025-12-30 19:07:07 +0300

    Velocity & Movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var my_variable = Vector2(10, 8)
-var speed = 300.0
+var speed = 50.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +9,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print(speed)
+func _physics_process(delta: float) -> void:
+	var direction = Vector2(-1,0)
+	
+	velocity = direction * speed
+	move_and_slide()
