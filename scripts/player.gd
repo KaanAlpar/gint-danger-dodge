d8477fc03cf226e0f6a55527cc8e0c763b87e3ca
Kaan Alpar <kaanalpar33@gmail.com>
2026-01-01 15:47:00 +0300

    Using Signals

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
+class_name Player
 
 var my_variable = Vector2(10, 8)
 var speed = 300.0
+var spawn_point = Vector2(60, 360)
 
 func _physics_process(_delta: float) -> void:
 	var direction = Vector2(0,0)
@@ -19,3 +21,6 @@ func _physics_process(_delta: float) -> void:
 	
 	velocity = direction * speed
 	move_and_slide()
+
+func reset_player():
+	global_position = spawn_point
